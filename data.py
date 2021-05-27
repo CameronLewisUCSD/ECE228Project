d05a5b3bdd9f824f85fad0d615501592142d3839
Cameron Lewis <ctl051@ucsd.edu>
2021-05-26 22:34:56 -0700

    Added preprocessing steps from RISCluster github, added example of how to get dataloader.

--- data.py
+++ data.py
@@ -10,7 +10,13 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import transforms
 from tqdm import tqdm
 
-
+#example:
+#dataset = H5SeismicDataset(
+#    filepath="/home/ctl051/228/Project/CustomProject/ECE228Project/data/Full.h5",
+#    transform = transforms.Compose(
+#        [SpecgramShaper(), SpecgramToTensor()]
+#    )
+#)
 class H5SeismicDataset(Dataset):
     """Loads samples from H5 dataset for use in native PyTorch dataloader."""
     def __init__(self, filepath, transform=None):
@@ -59,6 +65,40 @@ def getDataloaderSplit(H5Dataset, valid_perc,test_perc, batch_size=512, num_work
     DEC_loader_valid = DataLoader(H5Dataset, sampler=samplers[1], batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
     DEC_loader_test =  DataLoader(H5Dataset, sampler=samplers[2], batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
     return [DEC_loader_train,DEC_loader_valid, DEC_loader_test]
+
+class SpecgramShaper(object):
+    """Crop & reshape data."""
+    def __init__(self, n=None, o=None, transform='sample_norm_cent'):
+        self.n = n
+        self.o = o
+        self.transform = transform
+
+
+    def __call__(self, X):
+        if self.n is not None and self.o is not None:
+            N, O = X.shape
+        else:
+            X = X[:-1, 1:]
+        if self.transform is not None:
+            if self.transform == "sample_norm":
+                X /= np.abs(X).max(axis=(0,1))
+            elif self.transform == "sample_norm_cent":
+                # X = (X - X.mean(axis=(0,1))) / \
+                # np.abs(X).max(axis=(0,1))
+                X = (X - X.mean()) / np.abs(X).max()
+            else:
+                raise ValueError("Unsupported transform.")
+        else:
+            print("Test failed.")
+
+        X = np.expand_dims(X, axis=0)
+        return X
+
+
+class SpecgramToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, X):
+        return torch.from_numpy(X)
 
     
 
